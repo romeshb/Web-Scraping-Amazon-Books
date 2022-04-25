b427d8e52040e72999392f7015f68a3f8fbcc1bd
romeshb <borawakeromesh@gmail.com>
2022-04-25 07:42:56 +0530

    minor fixes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+from datetime import datetime
+from tqdm import tqdm
 
 def get_url(search_term):
     """Generates a URL from the search term."""
@@ -48,7 +50,7 @@ def main(search_term, level):
     records = []
     url = get_url(search_term)
 
-    for page in range(1, 21):
+    for page in tqdm(range(1, 21)):
         driver.get(url.format(page))
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         results = soup.find_all('div', {'data-component-type': 's-search-result'})
@@ -65,14 +67,18 @@ def main(search_term, level):
         # additional code for next level depth crawling and scraping product details, product description
         list_language = []
         list_book_description = []
-        for i in range(len(df['Link'])):
-            driver.get(df['Link'][i])
-            soup2 = BeautifulSoup(driver.page_source, 'html.parser')
-            book_description = soup2.find('div', {
-                "class": "a-expander-content a-expander-partial-collapse-content"}).text.strip()
-            product_details = soup2.find('div', {"id": "detailBullets_feature_div"})
-            language = product_details.find_all('span', {'class': 'a-list-item'})[1].find_all('span')[1].text.strip()
+        for i in tqdm(range(len(df['Link']))):
+            try:
+                driver.get(df['Link'][i])
+                soup2 = BeautifulSoup(driver.page_source, 'html.parser')
 
+                book_description = soup2.find('div', {
+                    "class": "a-expander-content a-expander-partial-collapse-content"}).text.strip()
+                product_details = soup2.find('div', {"id": "detailBullets_feature_div"})
+                language = product_details.find_all('span', {'class': 'a-list-item'})[1].find_all('span')[1].text.strip()
+            except:
+                language = 'Unknown'
+                book_description = 'Unknown'
             list_language.append(language)
             list_book_description.append(book_description)
 
@@ -84,4 +90,11 @@ def main(search_term, level):
 
 
 if __name__ == '__main__':
-    main('Books',0)
+    start_time = datetime.now()
+
+    # main('Books',0) # for product search results only
+    main('Books', 1)  # for product search results and individual product details
+
+    end_time = datetime.now()
+    print('Duration: {}'.format(end_time - start_time))
+
